f22742c131ef5fc427705722bc31f2cb3127e3f0
Siddhant Goel <siddhantgoel@gmail.com>
2013-05-18 21:15:16 +0200

    Get the results display working

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
     $("#query").focus();
     
-    var location = "https://api.stackexchange.com/2.1/search?site=stackoverflow";
+    var endpoint = "https://api.stackexchange.com/2.1/search?site=stackoverflow";
     
-    $.getJSON(location, { order: 'desc', max: 10, sort: 'votes', intitle: 'python generators'}).done(function(data) {
-        console.log(data);
-    })
+    $.getJSON(endpoint, {
+        order: 'desc',
+        max: 10,
+        sort: 'votes',
+        intitle: 'python generators'
+    }).done(function(data) {
+        if (data.items.length > 0) {
+            for (var i = 0; i < data.items.length; i++) {
+                $("#results > tbody:last").append([
+                    '<tr><td><a href="http://stackoverflow.com/q/',
+                    String(data.items[i].question_id),
+                    '">',
+                    data.items[i].title,
+                    '</a></td></tr>'
+                ].join(""));
+            }
+        }
+    });
 });
