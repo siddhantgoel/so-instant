3198336a3e567e990f470a134291e273ec168ec2
Siddhant Goel <siddhantgoel@gmail.com>
2013-05-19 12:33:59 +0200

    Part-integration of the StackExchange OAuth API

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
+    /* Initialization code */
     $("#query").focus();
-    
     $("#error_div").hide();
-    
     var endpoint = "https://api.stackexchange.com/2.1/search/advanced?site=stackoverflow";
+    var application_key = 'A3a8pTUXjWuDDLPQcZ8OSA((';
+    SE.init({
+        clientId: 1562,
+        key: application_key,
+        channelUrl: window.location.toString() + 'blank',
+        complete: function(data) {
+            console.log("SE API initialized");
+        }
+    });
     
+    /* Utility functions */
     var get_query = function() {
         var query = $("#query").val();
         if (query.length > 0) {
@@ -16,7 +25,6 @@ $(document).ready(function() {
             return '';
         }
     };
-    
     var pluralize = function(what, count) {
         if (count == 1) {
             return what;
@@ -24,13 +32,11 @@ $(document).ready(function() {
             return what + 's';
         }
     };
-    
     var capitalize = function(string) {
         if (string.length > 0) {
             return string.charAt(0).toUpperCase() + string.slice(1);
         }
     };
-    
     /* http://stackoverflow.com/a/1909508 */
     var delay = (function() {
         var timer = 0;
@@ -40,6 +46,7 @@ $(document).ready(function() {
         };
     })();
     
+    /* Update the table with the search query results */
     var update_results = function(data) {
         $("#results_tbody").empty();
         if (data.items.length > 0) {
@@ -58,6 +65,7 @@ $(document).ready(function() {
         }
     };
     
+    /* Fetch search results from the StackExchange API */
     var fetch = function() {
         var query = get_query();
         var data = $.jStorage.get(query);
@@ -69,7 +77,8 @@ $(document).ready(function() {
                 sort: 'relevance',
                 accepted: 'True',
                 q: query,
-                body: query
+                body: query,
+                key: application_key
             }).done(function(data) {
                 update_results(data);
                 $.jStorage.set(query, data, { TTL: 25000 });
@@ -82,6 +91,7 @@ $(document).ready(function() {
         }
     };
     
+    /* Bind the event handler for the text input box */
     $("#query").on("keyup", function(e) {
         if (typeof(e.which) == "number" && e.which > 0) {
             if (!(e.ctrlKey || e.metaKey || e.altKey) && !(e.keyCode >= 37 && e.keyCode <= 40)) {
@@ -92,6 +102,21 @@ $(document).ready(function() {
                 }
             }
         }
+    });
+    
+    /* Bind the login button to log in to StackExchange */
+    $("#signin").click(function() {
+        SE.authenticate({
+            success: function(data) {
+                console.log("Successfully logged in");
+                console.log(data);
+            },
+            error: function(data) {
+                console.log("Error with logging in");
+                console.log(data);
+            },
+            networkUsers: true
+        });
     });
     
 });
