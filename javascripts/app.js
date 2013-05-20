76dfd3947a6804bfef5005ac6b46d0e0ad247d2a
Siddhant Goel <siddhantgoel@gmail.com>
2013-05-20 10:40:56 +0200

    Add author name and time at which the question was asked

--- javascripts/app.js
+++ javascripts/app.js
@@ -61,12 +61,13 @@ $(document).ready(function() {
                 $("#results_table > tbody:last").append([
                     '<tr>',
                     data.items[i].is_answered ? '<td><i class="icon-ok answered"></i></td>' : '<td></td>',
-                    '<td><p><a href="' + data.items[i].link + '" target="_blank">' + data.items[i].title + '</a></p>',
+                    '<td><p><a href="' + data.items[i].link + '" target="_blank" class="name">' + data.items[i].title + '</a></p>',
                     '<p>',
                     $.map(data.items[i].tags, function(tag) {
                         return '<a class="btn btn-mini tag" target="_blank" href="http://stackoverflow.com/questions/tagged/' + tag + '">' + tag + '</a>';
                     }).join(" "),
                     '</p></td>',
+                    '<td>Asked ' + $.timeago(new Date(data.items[i].creation_date * 1000)) + ' by <a href="' + data.items[i].owner.link + '">' + data.items[i].owner.display_name + '</a></td>',
                     '<td>' + number_with_commas(data.items[i].answer_count) + ' ' + pluralize('answer', data.items[i].answer_count) + ' </td>',
                     '<td>' + number_with_commas(data.items[i].score) + ' ' + pluralize('vote', data.items[i].score) + ' </td>',
                     '<td>' + number_with_commas(data.items[i].view_count) + ' ' + pluralize('view', data.items[i].view_count) + ' </td>',
